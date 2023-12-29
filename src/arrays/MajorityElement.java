35c890d6b3e97e6da0546104e0dccbbddc5a4b61
sandypkumr <sandypkumr17@gmail.com>
2023-12-29 13:46:14 +0530

    3. Majority Element

---
+++ src/arrays/MajorityElement.java
@@ -0,0 +1,21 @@
+package arrays;
+
+public class MajorityElement {
+    public int majorityElement(int[] nums) {
+        int majElement = nums[0];
+        int rep = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (majElement == nums[i]) {
+                rep++;
+            } else {
+                if (rep == 0) {
+                    majElement = nums[i];
+                    rep++;
+                } else {
+                    rep--;
+                }
+            }
+        }
+        return majElement;
+    }
+}
