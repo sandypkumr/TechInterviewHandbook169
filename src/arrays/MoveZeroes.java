4105ed849ce6b60c2eb025db9ce95b610ba7361d
sandypkumr <sandypkumr17@gmail.com>
2024-01-03 19:47:26 +0530

    6. Move Zeroes

---
+++ src/arrays/MoveZeroes.java
@@ -0,0 +1,15 @@
+package arrays;
+
+public class MoveZeroes {
+    public void moveZeroes(int[] nums) {
+        int curNonZeroPos = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[curNonZeroPos++] = nums[i];
+            }
+        }
+        for (int i = curNonZeroPos; i < nums.length; i++) {
+            nums[i] = 0;
+        }
+    }
+}
