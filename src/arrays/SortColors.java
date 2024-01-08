4f83eace0bbba6f77e61698c4fb4ea5bc851314f
sandypkumr <sandypkumr17@gmail.com>
2024-01-08 16:23:12 +0530

    13. Sort Colors

---
+++ src/arrays/SortColors.java
@@ -0,0 +1,25 @@
+package arrays;
+
+public class SortColors {
+    public void sortColors(int[] nums) {
+        int red = 0, white = 0, blue = nums.length - 1;
+        while (white <= blue) {
+            if (nums[white] == 0) {
+                swap(nums, white, red);
+                red++;
+                white++;
+            } else if (nums[white] == 1) {
+                white++;
+            } else {
+                swap(nums, white, blue);
+                blue--;
+            }
+        }
+    }
+
+    private void swap(int[] nums, int index1, int index2) {
+        int temp = nums[index1];
+        nums[index1] = nums[index2];
+        nums[index2] = temp;
+    }
+}
