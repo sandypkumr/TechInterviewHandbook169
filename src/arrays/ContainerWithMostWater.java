8e4f8faae4962f2ee711bf549f5710c4f0e36c5f
sandypkumr <sandypkumr17@gmail.com>
2024-01-08 16:48:34 +0530

    14. Container With Most Water

---
+++ src/arrays/ContainerWithMostWater.java
@@ -0,0 +1,17 @@
+package arrays;
+
+public class ContainerWithMostWater {
+    public int maxArea(int[] height) {
+        int max = 0;
+        int start = 0, end = height.length - 1;
+        while (start < end) {
+            max = Math.max(max, (end - start) * Math.min(height[start], height[end]));
+            if (height[start] > height[end]) {
+                end--;
+            } else {
+                start++;
+            }
+        }
+        return max;
+    }
+}
