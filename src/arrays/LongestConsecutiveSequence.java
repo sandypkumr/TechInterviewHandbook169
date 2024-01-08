61b045bda14d04498d93730f6efc36f37c1b6614
sandypkumr <sandypkumr17@gmail.com>
2024-01-08 19:38:43 +0530

    16. Longest Consecutive Sequence

---
+++ src/arrays/LongestConsecutiveSequence.java
@@ -0,0 +1,22 @@
+package arrays;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LongestConsecutiveSequence {
+    public int longestConsecutive(int[] nums) {
+        Map<Integer, Integer> numMap = new HashMap<>();
+        int res = 0;
+        for (int num : nums) {
+            if (numMap.containsKey(num)) {
+                continue;
+            }
+            numMap.put(num, numMap.getOrDefault(num - 1, 0) +
+                    numMap.getOrDefault(num + 1, 0) + 1);
+            numMap.put(num - numMap.getOrDefault(num - 1, 0), numMap.get(num));
+            numMap.put(num + numMap.getOrDefault(num + 1, 0), numMap.get(num));
+            res = Math.max(res, numMap.get(num));
+        }
+        return res;
+    }
+}
