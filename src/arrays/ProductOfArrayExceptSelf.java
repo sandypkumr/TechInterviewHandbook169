55025d601efcfdbdc1a05a5bc36e605448290611
sandypkumr <sandypkumr17@gmail.com>
2024-01-08 09:24:46 +0530

    10. Product of Array Except Self

---
+++ src/arrays/ProductOfArrayExceptSelf.java
@@ -0,0 +1,18 @@
+package arrays;
+
+public class ProductOfArrayExceptSelf {
+    public int[] productExceptSelf(int[] nums) {
+        int[] res = new int[nums.length];
+        int pre = 1;
+        for (int i = 0; i < nums.length; i++) {
+            res[i] = pre;
+            pre *= nums[i];
+        }
+        pre = 1;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            res[i] *= pre;
+            pre *= nums[i];
+        }
+        return res;
+    }
+}
