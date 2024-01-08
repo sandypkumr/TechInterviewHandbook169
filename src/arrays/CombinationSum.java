45c5d7fa576b13c7214b607e80ccc6d594cf33cd
sandypkumr <sandypkumr17@gmail.com>
2024-01-08 14:12:27 +0530

    11. Combination Sum

---
+++ src/arrays/CombinationSum.java
@@ -0,0 +1,26 @@
+package arrays;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CombinationSum {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> resList = new ArrayList<>();
+        combinationSumHelper(0, new ArrayList<>(), 0, resList, candidates, target);
+        return resList;
+    }
+
+    private void combinationSumHelper(int index, List<Integer> curList, int total, List<List<Integer>> resList, int[] candidates, int target) {
+        if (total == target) {
+            resList.add(List.copyOf(curList));
+            return;
+        }
+        if (index == candidates.length || total > target) {
+            return;
+        }
+        curList.add(candidates[index]);
+        combinationSumHelper(index, curList, total + candidates[index], resList, candidates, target);
+        curList.removeLast();
+        combinationSumHelper(index + 1, curList, total, resList, candidates, target);
+    }
+}
