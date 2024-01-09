54c9f45d49e1feea4d18656e599b34574fe5d261
sandypkumr <sandypkumr17@gmail.com>
2024-01-09 18:13:27 +0530

    21. 3Sum Closest

---
+++ src/arrays/ThreeSumClosest.java
@@ -0,0 +1,30 @@
+package arrays;
+
+import java.util.Arrays;
+
+public class ThreeSumClosest {
+    public int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+        int minDiff = Integer.MAX_VALUE;
+        int midDiffSum = 0;
+        for (int i = 0; i < nums.length - 2; i++) {
+            int start = i + 1;
+            int end  = nums.length - 1;
+            while (start < end) {
+                int sum = nums[i] + nums[start] + nums[end];
+                if (Math.abs(sum - target) < minDiff) {
+                    minDiff = Math.abs(sum - target);
+                    midDiffSum = sum;
+                }
+                if (sum < target) {
+                    start++;
+                } else if (sum > target) {
+                    end--;
+                } else {
+                    return sum;
+                }
+            }
+        }
+        return midDiffSum;
+    }
+}
