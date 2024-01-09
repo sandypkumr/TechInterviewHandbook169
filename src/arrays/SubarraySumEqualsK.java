0131dc193d992a171e0f3986ca431ddcd27d3cb8
sandypkumr <sandypkumr17@gmail.com>
2024-01-09 08:50:18 +0530

    19. Subarray Sum Equals K

---
+++ src/arrays/SubarraySumEqualsK.java
@@ -0,0 +1,20 @@
+package arrays;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SubarraySumEqualsK {
+    public int subarraySum(int[] nums, int k) {
+        Map<Integer, Integer> resMap = new HashMap<>();
+        int sum = 0, res = 0;
+        resMap.put(0, 1);
+        for (int num : nums) {
+            sum += num;
+            if (resMap.containsKey(sum - k)) {
+                res += resMap.get(sum - k);
+            }
+            resMap.put(sum, resMap.getOrDefault(sum, 0) + 1);
+        }
+        return res;
+    }
+}
