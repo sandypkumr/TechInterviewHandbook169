0451cb38c0581de5078f7d938092a20aa66ff3e9
sandypkumr <sandypkumr17@gmail.com>
2024-01-09 18:50:44 +0530

    22. Non-overlapping Intervals

---
+++ src/arrays/NonOverlappingIntervals.java
@@ -0,0 +1,21 @@
+package arrays;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class NonOverlappingIntervals {
+    public int eraseOverlapIntervals(int[][] intervals) {
+        Arrays.sort(intervals, Comparator.comparing(o -> o[0]));
+        int res = 0;
+        int curHigh = Integer.MIN_VALUE;
+        for (int[] interval : intervals) {
+            if (interval[0] < curHigh) {
+                res++;
+                curHigh = Math.min(curHigh, interval[1]);
+            } else {
+                curHigh = interval[1];
+            }
+        }
+        return res;
+    }
+}
