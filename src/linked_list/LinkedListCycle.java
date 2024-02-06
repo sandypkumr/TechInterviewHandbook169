933f3624b38f7a24254dab7f0c1b4986d75fecf7
sandeepkumar.pabba <sandeepkumar.pabba@servicenow.com>
2024-02-06 07:17:12 +0530

    2.Linked List Cycle

---
+++ src/linked_list/LinkedListCycle.java
@@ -0,0 +1,19 @@
+package linked_list;
+
+public class LinkedListCycle {
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return false;
+        }
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while (fast != null && fast.next != null) {
+            if (slow == fast) {
+                return true;
+            }
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return false;
+    }
+}
